47dee7b7abc826a3ac437e07faaa52bc3eead85c
Seb Karena <EKChikui@gmail.com>
2024-01-27 21:27:35 -0300

    Update anscombe_quartet.py

--- 204_ascombes_quartet/anscombe_quartet.py
+++ 204_ascombes_quartet/anscombe_quartet.py
@@ -6,83 +6,166 @@
 
 
 # Libraries
+from collections import namedtuple
+
 import numpy as np
 import pandas as pd
+
+from sklearn.metrics import r2_score
 
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
 
-# Setup/Config
+# Functions
+def load_dataset():
+    """
+    Creates the Anscombe's dataset.
+
+    """
+    data1 = [[10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5],
+             [8.04, 6.95, 7.58, 8.81, 8.33, 9.96, 7.24, 4.26, 10.84, 4.82, 5.68]]
+
+    data2 = [[10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5],
+             [9.14, 8.14, 8.74, 8.77, 9.26, 8.10, 6.13, 3.10, 9.13, 7.26, 4.74]]
+
+    data3 = [[10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5],
+             [7.46, 6.77, 12.74, 7.11, 7.81, 8.84, 6.08, 5.39, 8.15, 6.42, 5.73]]
+
+    data4 = [[8, 8, 8, 8, 8, 8, 8, 19, 8, 8, 8],
+             [6.58, 5.76, 7.71, 8.84, 8.47, 7.04, 5.25, 12.5, 5.56, 7.91, 6.89]]
+
+
+    data1 = np.transpose(np.array(data1))
+    data2 = np.transpose(np.array(data2))
+    data3 = np.transpose(np.array(data3))
+    data4 = np.transpose(np.array(data4))
+
+    data1 = pd.DataFrame(data=data1, columns=["x", "y"])
+    data2 = pd.DataFrame(data=data2, columns=["x", "y"])
+    data3 = pd.DataFrame(data=data3, columns=["x", "y"])
+    data4 = pd.DataFrame(data=data4, columns=["x", "y"])
+
+
+    return data1, data2, data3, data4
+
+
+def plot_anscombes(data1, data2, data3, data4):
+    """
+    Internal function for Anscombe's Quarte plot.
+
+    """
+    # RC Params
+    plt.rcParams["font.family"] = "Helvetica"
+    plt.rcParams["font.weight"] = 8
+    plt.rcParams["xtick.bottom"] = False
+    plt.rcParams["ytick.right"] = False
+    
+    # Figure
+    fig = plt.figure(figsize=[8, 4.5])
+    grd = gridspec.GridSpec(nrows=2, height_ratios=[1, 1], hspace=0.35,
+                            ncols=2, width_ratios=[1, 1], wspace=0.30)
+
+    title = "Anscombes Quartet"
+    fig.suptitle(title, fontsize=10, weight="bold")
+
+    ax0 = plt.subplot(grd[0, 0])                            # First Plot
+    ax1 = plt.subplot(grd[0, 1], sharex=ax0, sharey=ax0)    # Second Plot
+    ax2 = plt.subplot(grd[1, 0], sharex=ax1, sharey=ax1)    # Third Plot
+    ax3 = plt.subplot(grd[1, 1], sharex=ax2, sharey=ax2)    # Fourth Plot
+
+    # Plots
+    ax0.scatter(data1["x"], data1["y"], color="darkblue", edgecolor="white", zorder=20)
+    ax0.plot([2,20], [4,13], color="darkred", linewidth=1, zorder=21)
+
+    ax1.scatter(data2["x"], data2["y"], color="darkblue", edgecolor="white", zorder=22)
+    ax1.plot([2,20], [4,13], color="darkred", linewidth=1, zorder=23)
+
+    ax2.scatter(data3["x"], data3["y"], color="darkblue", edgecolor="white", zorder=24)
+    ax2.plot([2,20], [4,13], color="darkred", linewidth=1, zorder=25)
+
+    ax3.scatter(data4["x"], data4["y"], color="darkblue", edgecolor="white", zorder=26)
+    ax3.plot([2,20], [4,13], color="darkred", linewidth=1, zorder=27)
+
+
+    ax0.set_xlim([2, 20])
+    ax0.set_ylim([2, 14])
+
+    ax0.set_xlabel("x1", fontsize=8)
+    ax0.set_ylabel("y1", fontsize=8)
+    ax0.grid(axis="both", color="lightgrey", linestyle="--", linewidth=0.5)
+
+    ax1.set_xlabel("x2", fontsize=8)
+    ax1.set_ylabel("y2", fontsize=8)
+    ax1.grid(axis="both", color="lightgrey", linestyle="--", linewidth=0.5)
+
+    ax2.set_xlabel("x3", fontsize=8)
+    ax2.set_ylabel("y3", fontsize=8)
+    ax2.grid(axis="both", color="lightgrey", linestyle="--", linewidth=0.5)
+
+    ax3.set_xlabel("x4", fontsize=8)
+    ax3.set_ylabel("y4", fontsize=8)
+    ax3.grid(axis="both", color="lightgrey", linestyle="--", linewidth=0.5)
+
+    #plt.savefig(title, dpi=240)
+    plt.show()
+
+
+    return None
+
+
+def metrics(DataFrame, title=None):
+    """
+    Returns the metrics (measures) that Ascombe used to prove that
+    a visualization is important.  
+
+    """
+    # Data preparation
+    x = np.array(DataFrame["x"])
+    y = np.array(DataFrame["y"])
+
+    metr_tuple = namedtuple("metrics", ["x_mean", "y_mean", "x_var", "y_var", "a", "b", "corr", "r2"])
+
+    # Title
+    if(title != None):
+        print(f"   ****  {title}  ****")
+
+    # Metrics (from Ascombe)
+    x_mean = np.mean(x)
+    y_mean = np.mean(y)
+    x_var = np.var(x)
+    y_var = np.var(y)
+
+    b, a = np.polyfit(x, y, deg=1)
+    y_pred = [a + b * i for i in x]
+
+    corr = np.corrcoef(x, y)[1, 0]
+    r2 = r2_score(y, y_pred)
+
+    # Print metrics
+    print(f" >     Mean x: {x_mean:.3f}")
+    print(f" >     Mean y: {y_mean:.3f}")
+    print(f" > Variance x: {x_var:.3f}")
+    print(f" > Variance y: {y_var:.3f}")
+    print(f" >    Pearson: {corr:.3f}")
+    print(f" >   Lin Regr: {a:.3f} + {b:.3f} * x")
+    print(f" >  R Squared: {r2:.3f} \n")
+
+    # Response
+    metr = metr_tuple(x_mean, y_mean, x_var, y_var, a, b, corr, r2)
+
+
+    return metr
+    
 
 
 # Program
-data = [["I_x", "I_y", "II_x", "II_y", "III_x", "III_y", "IV_x", "IV_y"],
-        [10.0, 8.04, 10.0, 9.14, 10.0, 7.46, 8.0, 6.58],
-        [8.0, 6.95, 8.0, 8.14, 8.0, 6.77, 8.0, 5.76],
-        [13.0, 7.58, 13.0, 8.74, 13.0, 12.74, 8.0, 7.71],
-        [9.0, 8.81, 9.0, 8.77, 9.0, 7.11, 8.0, 8.84],
-        [11.0, 8.33, 11.0, 9.26, 11.0, 7.81, 8.0, 8.47],
-        [14.0, 9.96, 14.0, 8.10, 14.0, 8.84, 8.0, 7.04],
-        [6.0, 7.24, 6.0, 6.13, 6.0, 6.08, 8.0, 5.25],
-        [4.0, 4.26, 4.0, 3.10, 4.0, 5.39, 19.0, 12.50],
-        [12.0, 10.84, 12.0, 9.13, 12.0, 8.15, 8.0, 5.56],
-        [7.0, 4.82, 7.0, 7.26, 7.0, 6.42, 8.0, 7.91],
-        [5.0, 5.68, 5.0, 4.74, 5.0, 5.73, 8.0, 6.89]]
+data1, data2, data3, data4 = load_dataset()
 
-df = pd.DataFrame(data=data[1: ], columns=data[0])
+plot_anscombes(data1, data2, data3, data4)
+
+for index, data in enumerate([data1, data2, data3, data4], start=1):
+    _ = metrics(data, title=f"Data {index}")
 
 
-# Ploting
-fig = plt.figure(figsize=[8, 4.5])
-
-plt.rcParams["font.family"] = "Helvetica"
-plt.rcParams["font.weight"] = 8
-plt.rcParams["xtick.bottom"] = False
-plt.rcParams["ytick.right"] = False
-
-
-grd = gridspec.GridSpec(nrows=2, height_ratios=[1, 1], hspace=0.35,
-                        ncols=2, width_ratios=[1, 1], wspace=0.30)
-
-title = "Anscombes Quartet"
-fig.suptitle(title, fontsize=10, weight="bold")
-
-ax0 = plt.subplot(grd[0, 0])                            # First Plot
-ax1 = plt.subplot(grd[0, 1], sharex=ax0, sharey=ax0)    # Second Plot
-ax2 = plt.subplot(grd[1, 0], sharex=ax1, sharey=ax1)    # Third Plot
-ax3 = plt.subplot(grd[1, 1], sharex=ax2, sharey=ax2)    # Fourth Plot
-
-ax0.scatter(df["I_x"], df["I_y"], color="darkblue", edgecolor="white", zorder=20)
-ax0.plot([2,20], [4,13], color="darkred", linewidth=1, zorder=21)
-
-ax1.scatter(df["II_x"], df["II_y"], color="darkblue", edgecolor="white", zorder=22)
-ax1.plot([2,20], [4,13], color="darkred", linewidth=1, zorder=23)
-
-ax2.scatter(df["III_x"], df["III_y"], color="darkblue", edgecolor="white", zorder=24)
-ax2.plot([2,20], [4,13], color="darkred", linewidth=1, zorder=25)
-
-ax3.scatter(df["IV_x"], df["IV_y"], color="darkblue", edgecolor="white", zorder=26)
-ax3.plot([2,20], [4,13], color="darkred", linewidth=1, zorder=27)
-
-ax0.set_xlim([2, 20])
-ax0.set_ylim([2, 14])
-
-ax0.set_xlabel("x1", fontsize=8)
-ax0.set_ylabel("y1", fontsize=8)
-ax0.grid(axis="both", color="lightgrey", linestyle="--", linewidth=0.5)
-
-ax1.set_xlabel("x2", fontsize=8)
-ax1.set_ylabel("y2", fontsize=8)
-ax1.grid(axis="both", color="lightgrey", linestyle="--", linewidth=0.5)
-
-ax2.set_xlabel("x3", fontsize=8)
-ax2.set_ylabel("y3", fontsize=8)
-ax2.grid(axis="both", color="lightgrey", linestyle="--", linewidth=0.5)
-
-ax3.set_xlabel("x4", fontsize=8)
-ax3.set_ylabel("y4", fontsize=8)
-ax3.grid(axis="both", color="lightgrey", linestyle="--", linewidth=0.5)
-
-#plt.savefig(title, dpi=240)
-plt.show()
+# end
