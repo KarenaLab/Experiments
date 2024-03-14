baf74450f6ffe32c0a772dc7118b1d51639c3066
Seb Karena <EKChikui@gmail.com>
2024-03-13 22:12:21 -0300

    Create experiment_lln.py

---
+++ 031_law_of_large_numbers/experiment_lln.py
@@ -0,0 +1,31 @@
+# Name [P031]
+# Experiment for the LLN (Law of Large Numbers)
+
+
+# Libraries
+import os
+import sys
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Personal modules
+sys.path.append(r"c:\python_modules")
+
+
+# Functions
+
+
+
+# Setup/Config
+
+
+
+# Program --------------------------------------------------------------
+
+
+
+
+# end
+
