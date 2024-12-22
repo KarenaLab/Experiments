86a960d5ac80ef3187d4c0f790008ccb4757cf5c
Seb Karena <EKChikui@gmail.com>
2024-12-22 00:18:29 -0300

    Create smoothing_techniques.py

---
+++ 449_us_labor_ts/smoothing_techniques.py
@@ -0,0 +1,24 @@
+# [P449] Smoothing techniques
+#
+
+
+# Libraries
+import numpy as np
+import pandas as pd
+
+
+# Functions
+
+
+    
+# moving average
+
+# exponential smoothing
+
+# holt_winters
+
+# lowess_smoothing
+
+# kalman_filter
+
+# savitzky_golay
