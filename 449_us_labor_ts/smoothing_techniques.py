e325f63b8bbdb82a49b02ccda7b9bd144fc13069
Seb Karena <EKChikui@gmail.com>
2024-12-22 00:23:12 -0300

    add `moving_average` function

--- 449_us_labor_ts/smoothing_techniques.py
+++ 449_us_labor_ts/smoothing_techniques.py
@@ -1,14 +1,24 @@
 # [P449] Smoothing techniques
 #
 
+# Versions
+# 01 - Dec 22rd, 2024 - Starter
+
 
 # Libraries
 import numpy as np
 import pandas as pd
 
 
-# Functions
+# ----------------------------------------------------------------------
+def moving_average(Series, window):
+    """
+    Performs the moving average with a given **window**.
 
+    """
+    data = Series.rolling(window=window).mean()
+
+    return data
 
     
 # moving average
