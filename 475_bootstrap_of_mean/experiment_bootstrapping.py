e441d8b6dfb1109c85dcc5b2b5ecabbda8eca8d8
Seb Karena <EKChikui@gmail.com>
2025-02-01 21:36:10 -0300

    Update experiment_bootstrapping.py

--- 475_bootstrap_of_mean/experiment_bootstrapping.py
+++ 475_bootstrap_of_mean/experiment_bootstrapping.py
@@ -1,10 +1,8 @@
-# [Pxxx] Project name
-# (optional) Short description of the program/module.
+# [P475] Bootstrapping with the mean
 
 
 # Libraries
 import os
-import sys
 
 import numpy as np
 import pandas as pd
@@ -14,20 +12,55 @@ import matplotlib.pyplot as plt
 
 
 # Personal modules
-sys.path.append(r"c:\python_modules")
+from bootstrap_mean import bootstrap_with_mean
 
 
 # Functions
+def load_gaussian(mean=0, stddev=1, size=1000, seed=None):
+    """
 
 
+    """
+    # Seed
+    if(isinstance(seed, int) == True):
+        np.random.seed(seed=seed)
+
+    # Data
+    data = np.random.normal(loc=mean, scale=stddev, size=size)
+
+
+    return data
+
+
+def data_split(data, size=25, seed=None):
+    """
+
+
+    """
+    # Data preparation
+    data = data.flatten()
+    sample_size = int(data.shape[0] * (size / 100))
+    print(sample_size)
+
+    # Sample selection
+    np.random.shuffle(data)
+    sample = data[0: sample_size]
+
+
+    return sample
+    
+    
 
 # Setup/Config
 
 
 
 # Program --------------------------------------------------------------
+data = load_gaussian(mean=5, stddev=1, size=2000, seed=314)
+pick = data_split(data, size=25)
 
-
+bootstrap = bootstrap_with_mean(pick, size=10, repeat=200)
+print(bootstrap)
 
 
 # end
