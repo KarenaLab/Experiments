20087f831b4acb448aae2aab25b7f5aed4474f4a
Seb Karena <EKChikui@gmail.com>
2025-02-15 23:01:17 -0300

    Update experiment_bootstrapping_contour.py

--- 476_bootstrap_of_mean/experiment_bootstrapping_contour.py
+++ 476_bootstrap_of_mean/experiment_bootstrapping_contour.py
@@ -86,7 +86,7 @@ def apply_minmax(array):
 # Program --------------------------------------------------------------
 data = load_gaussian(mean=50, stddev=5, size=2000, seed=314)
 
-size_space = np.linspace(start=1, stop=10, num=10, dtype=int)
+size_space = np.linspace(start=1, stop=15, num=15, dtype=int)
 repeat_space = np.linspace(start=5, stop=50, num=10, dtype=int)
 
 sample = data_split(data, size=np.max(repeat_space))
@@ -101,13 +101,33 @@ for size in size_space:
         mean_map.loc[size, repeat] = bs["bootstrap_mean"]
         range_map.loc[size, repeat] = bs["ci_upper"] - bs["ci_lower"]
 
+
 x, y = np.meshgrid(range_map.index, range_map.columns)
 z = np.array(range_map).T
 
+# Plot
+# Plot config
+plt.rcParams["font.family"] = "Helvetica"
+plt.rcParams["font.size"] = 8
+plt.rcParams["figure.dpi"] = 120
+plt.rcParams["ps.papersize"] = "A4"
+plt.rcParams["xtick.direction"] = "inout"
+plt.rcParams["ytick.direction"] = "inout"
+plt.rcParams["xtick.major.size"] = 3.5
+plt.rcParams["ytick.major.size"] = 3.5
+
+# Plot figure 
+fig = plt.figure(figsize=[8, 4.5])
+fig.suptitle("Bootstrap - Countour of the Interval Confidence range",
+             fontweight="bold", x=0.98, ha="right")
 
 cs1 = plt.contour(x, y, z, linewidths=0.5, colors='k')
 cs2 = plt.contour(x, y, z, cmap="RdBu_r")
 plt.clabel(cs1, fontsize=7)
+
+plt.xlabel("percentage of sub-sample size (%)", loc="right")
+plt.ylabel("Number of repetitions (r)", loc="top")
+
 
 plt.show()
 
